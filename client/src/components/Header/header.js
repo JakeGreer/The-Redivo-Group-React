532ad4bdee9b09880496bf99148871c3a5ca4df5
theRealScoobaSteve <stephensimone@live.com>
2018-01-31 17:23:53 -0800

    Adding header

--- client/src/components/Header/header.js
+++ client/src/components/Header/header.js
@@ -7,35 +7,40 @@ class Header extends Component {
     render() {
         return (
             <div className="header">
-                <div className="logo clearfix">
-                    <a id="logo"><img src={Logo} className="logo-img img-responsive top-logo" /></a>
-                </div>
-                <div className="top">
-                    <div className="container">
-                        <div className="tabLine">
-                            209 AVENIDA DEL MAR | SUITE 104 | SAN CLEMENTE, CALIFORNIA
-                        </div>
-                        <div className="phoneNum">
-                            <i className="fa fa-phone"></i>949 838-5449
+                <div className="row">
+                    <div className="top col-md-12">
+                        <div className="container">
+                            <div className="row">
+                                <div className="col-md-8  main-color-txt address-header text-center">
+                                    209 AVENIDA DEL MAR | SUITE 104 | SAN CLEMENTE, CALIFORNIA
+                                </div>
+                                <div className="col-md-4 phone main-color-txt phone" >
+                                    <i className="fa fa-phone"></i>949 838-5449
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div className="clearfix"></div>
-                <div className="bottom">
-                    <ul class="nav justify-content-center">
-                        <li class="nav-item">
-                            <a class="nav-link active" href="#">Active</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Link</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Link</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Disabled</a>
-                        </li>
-                    </ul>
+                <div className="row">
+                    <div className="bottom col-md-12">
+                        <div className="logo">
+                            <a id="logo"><img src={Logo} className="logo-img img-responsive" /></a>
+                        </div>
+                        <ul class="nav justify-content-center">
+                            <li class="nav-item">
+                                <a class="nav-link active" href="#">Active</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Link</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Link</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Disabled</a>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         )
