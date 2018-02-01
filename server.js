cd7e28e899e9387ea978cfd71c9f8434d0b1a360
Jake Greer <jakegreer11@gmail.com>
2018-01-31 16:30:22 -0800

    added the basic file structure

---
+++ server.js
@@ -0,0 +1,51 @@
+// *****************************************************************************
+// Server.js - This file is the initial starting point for the Node/Express server.
+//
+// ******************************************************************************
+// *** Dependencies
+// =============================================================--------------------------------------
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const env = require('dotenv').load();
+var morgan = require('morgan');
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Set up promises with mongoose
+mongoose.Promise = global.Promise;
+// Connect to the Mongo DB
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://127.0.0.1/project-lfg",
+  {
+    useMongoClient: true
+  }
+);
+
+// Sets up the Express App
+// =============================================================
+app.use('/public', express.static('public')) // Static directory
+app.use(morgan('dev')); // log every request to the console
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+
+// Routes
+// =============================================================
+const routes = require("./routes")
+app.use(routes);
+
+// Send every request to the React app
+// Define any API routes before this runs
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+app.listen(PORT, function() {
+  console.log(`🌎 ==> Server now on port ${PORT}!`);
+});
