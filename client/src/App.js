a1b4c38798e4b965a0722f224fcebd214299da39
theRealScoobaSteve <stephensimone@live.com>
2018-02-02 15:45:53 -0800

    Added the beginning of the properties page

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import Header from "./components/Header";
-import './App.css';
+import SelectorBtn from "./components/Selector-Button"
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <SelectorBtn />
       </div>
     );
   }
---
+++ client/src/components/Selector-Button/SelectorBtn.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./SelectorBtn.css";
+import { Link } from "react-router-dom"
+
+const SelectorBtn = () =>
+{
+    return (
+        <div>
+            {/*<Link className="area-selector" />*/}
+        </div>
+    )
+}
+
+export default SelectorBtn;
