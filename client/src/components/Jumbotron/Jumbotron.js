0740f85257234e79ee8b4f92942615d8982d1855
Jake Greer <jakegreer11@gmail.com>
2018-02-02 14:01:43 -0800

    server wont start

---
+++ client/src/components/Jumbotron/Jumbotron.js
@@ -0,0 +1,21 @@
+import React from "react";
+import './Jumbotron.css';
+
+const Header = () => (
+
+    <header>
+        <div className="jumbotron text-center">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-12">
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+)
+
+
+export default Header;
+
