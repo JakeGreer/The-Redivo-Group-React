53c349b530cd932ccc66ce2f4a43fbe2669abad1
theRealScoobaSteve <stephensimone@live.com>
2018-02-02 16:05:47 -0800

    Fixed app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,13 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import Header from "./components/Header"
 import Carousel from "./components/Carousel"
-import './App.css';
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header />
-        <SelectorBtn />
-        <Carousel />
->>>>>>> master
+
       </div>
     );
   }
