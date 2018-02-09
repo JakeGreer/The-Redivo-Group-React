ffeaa938831b52a54682dff5a3e118269671feb3
Jake Greer <jakegreer11@gmail.com>
2018-02-09 15:42:14 -0800

    added the routes and controller folder and the propertyDetail component.

---
+++ client/src/components/PropertyDetail/PropertyDetail.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "PropertyDetail.css"
+
+const PropertyDetail = (props) => {
+    return(
+        <div className="col-md-12">
+        </div>
+    )
+}
+
+export default PropertyDetail
