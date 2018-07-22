2f39ba99955b5a686ef111ef8768de55ac01f3dd
theRealScoobaSteve <stephensimone@live.com>
2018-07-22 14:34:39 -0700

    Added slide

--- client/src/components/Arrow/Arrow.jsx
+++ client/src/components/Arrow/Arrow.jsx
@@ -3,7 +3,9 @@ import React from "react";
 const Arrow = ({ size, direction }) => {
   return (
     <div>
-      <ion-icon size={size} name={`arrow-drop${direction}-circle`} />
+      <a>
+        <ion-icon size={size} name={`arrow-drop${direction}-circle`} />
+      </a>
     </div>
   );
 };
--- client/src/pages/Properties/LocalHomeSlide/LocalHomeSlide.jsx
+++ client/src/pages/Properties/LocalHomeSlide/LocalHomeSlide.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import Arrow from "../../../components/Arrow";
+import "./style.css";
 
 const LocalHomeSlide = () => {
   return (
-    <div className="row">
-      <div className="col-1">
-        <Arrow size="large" direction="left" />
-      </div>
-      <div className="col-1">
-        <Arrow size="large" direction="right" />
+    <div id="local-home-slide">
+      <div className="row">
+        <div className="col-1">
+          <Arrow size="large" direction="left" />
+        </div>
+        <div className="col-10" />
+        <div className="col-1">
+          <Arrow size="large" direction="right" />
+        </div>
       </div>
     </div>
   );
