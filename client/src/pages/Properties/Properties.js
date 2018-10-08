d093dfb17c28b233ba82d8599dfc25afa227ddd9
theRealScoobaSteve <stephensimone@live.com>
2018-10-08 16:00:25 -0700

    Fixed merge conflict

--- client/src/pages/Properties/Properties.js
+++ client/src/pages/Properties/Properties.js
@@ -65,10 +65,7 @@ class Properties extends Component {
   render() {
     return (
       <div>
-<<<<<<< HEAD
-=======
         {/* <LocalHomesSlide /> */}
->>>>>>> a14224cf9d6ab6600ee47a77af1db07b55881e66
         <SearchMap />
         <LocalHomesSlide />
       </div>
